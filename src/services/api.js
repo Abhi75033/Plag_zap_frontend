f8c539e71199055b02690266177ce658155a8632
Abhishek Kumar <abhishekyadav1112.21@gmail.com>
2025-12-07 04:45:15 +0530

    vb

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://plagzap-backend-2.onrender.com/api',
 });
 
 // Add auth token to requests
