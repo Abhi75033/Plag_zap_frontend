a4548501e7d40cae0f5efca9f6fecdab05f31183
Abhishek Kumar <abhishekyadav1112.21@gmail.com>
2025-12-23 20:09:18 +0530

    fixed for railway

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://plagzapbackend-production.up.railway.app//api',
+    baseURL: 'https://plagzapbackend-production.up.railway.app/api',
 });
 
 // Add auth token to requests
